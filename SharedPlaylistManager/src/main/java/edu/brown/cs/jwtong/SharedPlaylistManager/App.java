8b05c0720571591d074ec078eb67d529fd9e2727
JeremyTong Linux <Jeremy_Tong@brown.edu>
2015-03-29 14:16:54 -0500

    Create Maven Project

---
+++ SharedPlaylistManager/src/main/java/edu/brown/cs/jwtong/SharedPlaylistManager/App.java
@@ -0,0 +1,13 @@
+package edu.brown.cs.jwtong.SharedPlaylistManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
